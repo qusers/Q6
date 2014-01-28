9e8e30690bf2319b021792bd2e82aa36257c8eb1
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2014-01-28 10:15:15 +0100

    added scripts and pre-history of Q to folder hierarchy

---
+++ scripts/charmm36par_2_qpar.pl
@@ -0,0 +1,117 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+open OUT , ">qcharmm36_prot.prm" or die $!;
+select OUT;
+
+print "*------------------------------------------------\n";
+print "*\n";
+print "*Q-FF parameters: CHARMM27 parameters\n";
+print "*------------------------------------------------\n";
+print "[options] force-field options\n";
+print "name            Q-charmm27 nucleic acids\n";
+print "vdw_rule    arithmetic   ! vdW combination rule (geometric or arithmetic )\n";
+print "scale_14         1.0         ! electrostatic 1-4 scaling factor\n";
+print "switch_atoms  off	! on = use switch atom; off = use charge group\n";
+print "improper_definition explicit ! improper representation by 2 or four atoms\n";
+print "improper_potential	harmonic\n";
+print "coulomb_constant 332.0716    ! Constant in electrostatic energy calculation; default = 332.\n";
+print "force_field CHARMM           ! Force Field Type (GROMOS (default), AMBER or CHARMM)\n\n";
+print "[atom_types] atom type definitions\n";
+print "*-iac------Avdw1------Avdw2-----Bvdw1------Avdw3-----Bvdw2&3----mass---SYBYL-name-old-comment\n";
+
+open IN1 , "<top_all36_prot.rtf" or die $!;
+
+my $counter1 = 0;
+my @atomtype;
+my @mass;
+
+while (<IN1>) {
+     if ($_ =~ /^(MASS)\s+[0-9]+\s+(\w+)\s+([0-9]+\.[0-9]+)/) {
+          $atomtype[$counter1] = $2;
+          $mass[$counter1] = $3;
+          $counter1++;
+     }
+}
+
+open IN2 , "<par_all36_prot.prm" or die $!;
+
+while (<IN2>) {
+      if ($_ =~ /^(\w+)\s+0\.0\s+(\-[0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)/) {
+          for my $i (0..$#atomtype) {
+                 if ("$atomtype[$i]" eq "$1") {
+                     printf "%-10s %-10.4f %-9.4f %-10.4f %-9.4f %-9.4f %6.3f %1.0f\n" , "$1" , $3 , $3 , (-1*$2) , $3 , (-1*$2) , $mass[$i] , 1;
+                 }
+          }
+      }
+}
+
+print "*--------------------------------------------------\n\n";
+print "[bonds] types definitions\n";
+print "*iaci iacj   force.c.  dist.\n";
+print "*------------------------------------------------\n";
+
+open IN2 , "<par_all36_prot.prm" or die $!;
+
+while (<IN2>) {
+      if ($_ =~ /^(\w+)\s+(\w+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)/) {
+          printf "%-7s %-9s %8.3f %7.3f\n" , "$1" , "$2" , (2*$3) , $4;
+      }
+}
+
+print "*------------------------------------------------\n\n";
+print "[angles] angle type definitions\n";
+print "* iaci iacj iack  forceK angle0\n";
+print "*------------------------------------------------\n";
+
+open IN2 , "<par_all36_prot.prm" or die $!;
+
+while (<IN2>) {
+      if ($_ =~ /^(\w+)\s+(\w+)\s+(\w+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s*([0-9]+\.[0-9]+)?\s*([0-9]+\.[0-9]+)?/) {
+          if ($6) {
+             printf "%-7s %-7s %-9s %6.3f %7s %6s %10s\n" , "$1" , "$2" , "$3" , (2*$4) , "$5" , "$6" , "$7";
+          }
+          else {
+             printf "%-7s %-7s %-9s %6.3f %7s %6s %10s\n" , "$1" , "$2" , "$3" , (2*$4) , "$5" , "0.00" , "0.00000";
+          }
+      }
+}
+
+print "*------------------------------------------------\n\n";
+print "[torsions] torsion type definitions\n";
+print "*iaci iacj iack iacl  forceK  #minima phase   #paths\n";
+print "*------------------------------------------------\n";
+
+open IN2 , "<par_all36_prot.prm" or die $!;
+
+while (<IN2>) {
+     if ($_ =~ /^(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s+([0-9]+\.[0-9]+)\s+([0-9]+)\s+(\-?[0-9]+\.[0-9]+)/) {
+          last if "$2" eq "X";
+          if ("$1" eq "X") {
+              printf "%-7s %-7s %-7s %-10s %-5.3f %7.3f %8.3f %5.0f\n" , "?" , "$2" , "$3" , "?" , $5 , $6 , $7 , 1;
+          }
+          else {
+              printf "%-7s %-7s %-7s %-10s %-5.3f %7.3f %8.3f %5.0f\n" , "$1" , "$2" , "$3" , "$4" , $5 , $6 , $7 , 1;
+          }
+     }
+}
+
+print "*------------------------------------------------\n\n";
+print "[impropers] improper torsion type definitions\n";
+print "*iaci iacj forceK   imp0\n";
+print "*------------------------------------------------\n";
+
+open IN2 , "<par_all36_prot.prm" or die $!;
+
+while (<IN2>) {
+    if ($_ =~ /(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s+(\-?[0-9]+\.[0-9]+)\s+(0)\s+([0-9]+\.[0-9]+)/) {
+         if ("$2" eq "X") {
+              printf "%-7s %-7s %-7s %-8s %7.3f %8.3f\n" , "?" , "$1" , "$4" , "?" , (2*$5) , $7;
+         }
+         else {
+              printf "%-7s %-7s %-7s %-8s %7.3f %8.3f\n" , "$1" , "$2" , "$3" , "$4" , (2*$5) , $7;
+         }
+    }
+}
+print "*------------------------------------------------\n";
---
+++ scripts/charmm36top_2_qtop.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+use strict; 
+use warnings;
+
+open IN , "<top_all36_prot.rtf" or die $!;
+open OUT , ">qcharmm36_prot.qlib" or die $!;
+select OUT;
+print "*------------------------------------------------------------------\n";
+print "*\n";
+print "* Q residue library file: CHARMM27 nucleic acid library\n";
+print "*\n";
+
+my $activate_first = 1;
+my $activate_second = 0;
+my $activate_third = 0;
+my $label = 1;
+my $counter1 = 0;
+my $counter2=1;
+my $memoriser = 0;
+my $res = "";
+my @charge_groups;
+
+while (<IN>) {
+   $activate_first = 0 if $_ =~ /^(PRES)/;
+   $activate_first = 1 if $_ =~ /\w+\s+(TIP3|SOD|MG)/;
+   if ($_ =~ /^(RESI)\s+(\w+)/ and $activate_first) {
+      print "*------------------------------------------------------------------\n";
+      $label = 1;
+      $activate_second = 1;
+      $activate_third = 1;
+      print "{$2}\n";
+      printf "%3s %-6s\n" , "" , "[info]";
+      printf "%7s %-10s %-11s %-6s %-12s %-4s\n" , "" , "SYBYLtype" , "RESIDUE" , "!SYBYL" , "substructure" , "type";
+      $res = "$2";
+   }
+   if ($_ =~ /^(ATOM|GROUP)\s+(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*(\-?[0-9]+\.[0-9]+)?/ and $activate_first) {
+      printf "%10.0f %-6s %-11s %6.3f\n", $label , "$2" , "$3" , $4 if $2 and $3 and $4;
+      $charge_groups[$counter1] = $2 if $2;
+      $charge_groups[$counter1] = $1 if "$1" eq "GROUP";
+      $counter1++;
+      $label++ unless "$1" eq "GROUP";
+   }
+   if ($_ =~ /^(BOND)\s+(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*(\w+\'?\'?)?\s*/ and $activate_first) {
+      printf "%11s\n" , "[bonds]" if $activate_second == 1;
+      $activate_second = 0;
+      printf "%6s %-4s %-17s %15s\n" , "" , "$2" , "$3" , "!connect i -- j" if $2 and $3;
+      printf "%6s %-4s %-17s %15s\n" , "" , "$4" , "$5" , "!connect i -- j" if $4 and $5;
+      printf "%6s %-4s %-17s %15s\n" , "" , "$6" , "$7" , "!connect i -- j" if $6 and $7;
+      printf "%6s %-4s %-17s %15s\n" , "" , "$8" , "$9" , "!connect i -- j" if $8 and $9;
+      printf "%6s %-4s %-17s %15s\n" , "" , "$10" , "$11" , "!connect i -- j" if $10 and $11;
+      $counter2++;
+   }
+   if ($_ =~ /^(IMPH)\s+(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s*(\w+)?\s*(\w+)?\s*(\w+)?\s*(\w+)?\s*(\w+)?\s*(\w+)?\s*(\w+)?\s*(\w+)?\s/ and $activate_first) {
+       printf "%17s %35s\n" , "[connections]" , "!how to bond to previous and next" if $activate_third;
+       printf "%12s %3s %-3s\n" , "head" , "" , "P" if $activate_third;
+       printf "%12s %3s %-3s\n" , "tail" , "" , "O3'" if $activate_third;  
+       printf "%15s\n" , "[impropers]" if $activate_third;  
+       printf "%10s %6s %6s %6s\n" , "$2" , "$3" , "$4" , "$5";
+       printf "%10s %6s %6s %6s\n" , "$6" , "$7" , "$8" , "$9" if $6 and $7 and $8 and $9;
+       printf "%10s %6s %6s %6s\n" , "$10" , "$11" , "$12" , "$13" if $10 and $11 and $12 and $13;
+       printf "%58s\n" , "[charge_groups] !charge groups, with switch atom first" if $activate_third and $memoriser !=75;
+       if ($activate_third and $memoriser != 75) {
+          for my $i ($memoriser..$#charge_groups) {
+               print "    $charge_groups[$i]" unless "$charge_groups[$i]" eq "GROUP";
+               print "\n" if "$charge_groups[$i]" eq "GROUP";                 
+               last if "$charge_groups[$i]" eq "O3'";
+          }
+          $memoriser += $label+3 if $activate_third;
+          print "\n";
+       }
+       if ($activate_third == 0 and $memoriser == 75) {
+           printf "%58s\n" , "[charge_groups] !charge groups, with switch atom first";
+           for my $i ($memoriser..$#charge_groups) {
+               print "    $charge_groups[$i]" unless "$charge_groups[$i]" eq "GROUP";
+               print "\n" if "$charge_groups[$i]" eq "GROUP";                 
+               last if "$charge_groups[$i]" eq "O3'";
+           }
+          $memoriser += $label+3;
+          print "\n";
+       }
+       $activate_third = 0;
+   }
+   if ("$_" =~ /PATCHING/) {
+        printf "%58s\n" , "[charge_groups] !charge groups, with switch atom first" if "$res" eq "TIP3" or "$res" eq "SOD" or "$res" eq "MG";
+        print "    OH2    H1    H2\n" if "$res" eq "TIP3";
+        print "    SOD\n" if "$res" eq "SOD"; 
+        print "    MG\n"  if "$res" eq "MG";
+   }
+}
+print "*------------------------------------------------------------------\n";
