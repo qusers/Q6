d78ce4da9bedd9881828ce860b3db7ec42980523
Paul Bauer <paul.bauer@icm.uu.se>
2014-11-02 19:17:07 +0200

    Quick fix for qcalc, there was a missing array for the FEP file loading in calc_base.f90, fixed now Sorry for the screw-up

--- src/calc_base.f90
+++ src/calc_base.f90
@@ -31,7 +31,7 @@ character					::	libtext*80,qaname*2
 integer					::	i,j,k,iat
 !temp. array for reallocating long-range exclusion list
 integer(AI), pointer	::	tempexlong(:,:)
-
+	allocate(iqatom(nat_pro))
 ! --- # states, # q-atoms
 if(.not. qatom_load_atoms(fep_file)) then
         call die_general('failure to load Q-atoms from FEP file.')
