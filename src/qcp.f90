008c80ed15647aafcd0bd6dcc305b0a05702d108
Paul Bauer <paul.bauer@icm.uu.se>
2016-04-01 07:42:58 +0200

    Initial commit for QCP branch
    Started work on reimplementing the Quantum classical path module in Q.
    First work on preparing the ground, real stuff comes later.

---
+++ src/qcp.f90
@@ -0,0 +1,14 @@
+! (C) 2014 Uppsala Molekylmekaniska HB, Uppsala, Sweden
+! qcp.f90
+! Q is written by Johan Åqvist, John Marelius, Anders Kaplan, Isabella Feierberg, Martin Nervall & Martin Almlöf
+! Quantum classical path module
+! written by Paul Bauer
+
+module QCP
+
+use TOPO
+use QATOM
+use SIZES
+use NRGY
+
+end module QCP
