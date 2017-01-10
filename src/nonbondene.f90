264b6fe5e12ac94cb58cb1a8098ef1063f8c294c
acmnpv <paul.bauer@icm.uu.se>
2017-01-10 15:23:04 +0100

    Change to collection of LRF data in MPI
    The function to collect and combine the MPI data from the nodes has been
    changed to remove the MPI_IN_PLACE option. This could case segmentation
    faults when using Link Time Optimization with certain GCC/MPI
    implementations (likely because GCC wanted to remove something it
    shouldn't). Now the function copies the buffer first to a temporary data
    structure before setting LRF to the final values.

--- src/nonbondene.f90
+++ src/nonbondene.f90
@@ -618,10 +618,12 @@ end function lrf_cgp_rep
 ! only used with MPI
 #ifdef USE_MPI
 subroutine lrf_gather
-call MPI_Allreduce(MPI_IN_PLACE,LRF,ncgp,mpitype_batch_lrf,mpi_lrf_add,&
+! need this to keep some stuff from crashing with MPI_IN_PLACE option
+type(LRF_TYPE) :: MPI_LRF_TMP(ncgp)
+call MPI_Allreduce(LRF,MPI_LRF_TMP,ncgp,mpitype_batch_lrf,mpi_lrf_add,&
 MPI_COMM_WORLD,ierr)
 if (ierr .ne. 0) call die('lrf_gather reduce')
-!if (nodeid .eq.0 ) LRF = lrf_reduce
+LRF = MPI_LRF_TMP
 end subroutine lrf_gather
 #endif
 
